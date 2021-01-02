477f9f0da3b1199b4f41cdb0800b899db7841096
devggaurav <devggaurav@gmail.com>
2021-01-02 19:28:15 +0530

    vuetify todo

---
+++ js/about.c4d5bc97.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about pa-6"},[n("h1",[t._v("This is an about page")]),n("p",[t._v("Lorem Ipsum")])])}],u=n("2877"),r={},c=Object(u["a"])(r,a,s,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.c4d5bc97.js.map
